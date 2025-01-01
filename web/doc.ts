58c3359b740b6785c22570cffcaf6a430ad69394
CJ Cummings <cj@formata.io>
2025-01-01 11:22:48 -0700

    add types

--- web/doc.ts
+++ web/doc.ts
@@ -14,6 +14,7 @@
 // limitations under the License.
 //
 
+// @deno-types="./pkg/stof.d.ts"
 import init, { type StofData, StofDoc, type StofField, type StofFunc, type StofNode } from './pkg/stof.js';
 
 
