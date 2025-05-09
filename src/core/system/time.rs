efb5d8d78405dba97fef3b8be3dfdba908158169
CJ Cummings <cj@formata.io>
2025-05-09 17:43:14 -0600

    v0.3.38 + time to/from RFC 2822 & 3339

--- src/core/system/time.rs
+++ src/core/system/time.rs
@@ -15,6 +15,7 @@
 //
 
 use std::{ops::Deref, time::{Duration, SystemTime, UNIX_EPOCH}};
+use chrono::{DateTime, Utc};
 use crate::{lang::SError, Library, SDoc, SNum, SUnits, SVal};
 
 
@@ -165,6 +166,98 @@ impl Library for TimeLibrary {
                     _ => {}
                 }
                 Err(SError::time(pid, &doc, "sleepNano", "expecting a number to use for sleep"))
+            },
+
+            /*****************************************************************************
+             * RFC 3339.
+             *****************************************************************************/
+            // Return an RFC 3339 string representing the current time.
+            "nowRFC3339" => {
+                let now: DateTime<Utc> = DateTime::from(SystemTime::now());
+                Ok(SVal::String(now.to_rfc3339()))
+            },
+            // Return an RFC 2822 string representing the current time.
+            "nowRFC2822" => {
+                let now: DateTime<Utc> = DateTime::from(SystemTime::now());
+                Ok(SVal::String(now.to_rfc2822()))
+            },
+            // Return an RFC 3339 string representing the given Unix timestamp (time since unix epoch, millisecond accuracy).
+            "toRFC3339" => {
+                if parameters.len() > 0 {
+                    match &parameters[0] {
+                        SVal::Number(num) => {
+                            let ms = num.float_with_units(SUnits::Milliseconds).abs() as i64;
+                            if let Some(time) = DateTime::from_timestamp_millis(ms) {
+                                return Ok(SVal::String(time.to_rfc3339()));
+                            }
+                        },
+                        SVal::Boxed(val) => {
+                            let val = val.lock().unwrap();
+                            let val = val.deref();
+                            match val {
+                                SVal::Number(num) => {
+                                    let ms = num.float_with_units(SUnits::Milliseconds).abs() as i64;
+                                    if let Some(time) = DateTime::from_timestamp_millis(ms) {
+                                        return Ok(SVal::String(time.to_rfc3339()));
+                                    }
+                                },
+                                _ => {}
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+                Err(SError::time(pid, &doc, "toRFC3339", "expecting a timestamp to convert"))
+            },
+            // Return an RFC 2822 string representing the given Unix timestamp (time since unix epoch, millisecond accuracy).
+            "toRFC2822" => {
+                if parameters.len() > 0 {
+                    match &parameters[0] {
+                        SVal::Number(num) => {
+                            let ms = num.float_with_units(SUnits::Milliseconds).abs() as i64;
+                            if let Some(time) = DateTime::from_timestamp_millis(ms) {
+                                return Ok(SVal::String(time.to_rfc2822()));
+                            }
+                        },
+                        SVal::Boxed(val) => {
+                            let val = val.lock().unwrap();
+                            let val = val.deref();
+                            match val {
+                                SVal::Number(num) => {
+                                    let ms = num.float_with_units(SUnits::Milliseconds).abs() as i64;
+                                    if let Some(time) = DateTime::from_timestamp_millis(ms) {
+                                        return Ok(SVal::String(time.to_rfc2822()));
+                                    }
+                                },
+                                _ => {}
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+                Err(SError::time(pid, &doc, "toRFC2822", "expecting a timestamp to convert"))
+            },
+            // Parse an RFC 3339 string into a unix timestamp (milliseconds since unix epoch).
+            "parseRFC3339" => {
+                if parameters.len() > 0 {
+                    let str = parameters[0].to_string();
+                    if let Ok(res) = DateTime::parse_from_rfc3339(&str) {
+                        let milli = res.timestamp_millis();
+                        return Ok(SVal::Number(SNum::Units(milli as f64, SUnits::Milliseconds)));
+                    }
+                }
+                Err(SError::time(pid, &doc, "parseRFC3339", "expecting a valid RFC 3339 string to parse"))
+            },
+            // Parse an RFC 2822 string into a unix timestamp (milliseconds since unix epoch).
+            "parseRFC2822" => {
+                if parameters.len() > 0 {
+                    let str = parameters[0].to_string();
+                    if let Ok(res) = DateTime::parse_from_rfc2822(&str) {
+                        let milli = res.timestamp_millis();
+                        return Ok(SVal::Number(SNum::Units(milli as f64, SUnits::Milliseconds)));
+                    }
+                }
+                Err(SError::time(pid, &doc, "parseRFC2822", "expecting a valid RFC 2822 string to parse"))
             },
             _ => {
                 Err(SError::time(pid, &doc, "NotFound", &format!("{} is not a function in the Time Library", name)))
